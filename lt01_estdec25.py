a4c0550d41beaa3b2270479feabda1019b59b43f
goacode <trabalhogustavoandrade@gmail.com>
2025-08-28 21:30:24 -0300

    exercicio 025

---
+++ lt01_estdec25.py
@@ -0,0 +1,12 @@
+hora_inicio = int(input("Hora Inicio: "))
+min_inicio = int(input("Minuto Inicio: "))
+
+hora_fim = int(input("Hora Fim: "))
+min_fim = int(input("Minuto Fim: "))
+
+
+
+
+print(f"{hora_jogo}h {min_jogo}min")
+
+
